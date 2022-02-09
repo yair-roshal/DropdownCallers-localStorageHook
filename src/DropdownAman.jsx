9d76ed3e8841c7f1364a8dd302678ee8679c2316
[yair-r] <yair7705778@gmail.com>
2022-02-09 15:06:01 +0200

    end

--- src/DropdownAman.jsx
+++ src/DropdownAman.jsx
@@ -1,16 +1,15 @@
-import {useState} from "react"
+import {useState, useRef, useEffect, useLayoutEffect} from "react"
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import {useLocalStorage} from './hooks/useLocalStorage'
 
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, {SelectChangeEvent} from '@mui/material/Select';
+import Select from '@mui/material/Select';
 
 const initialData = [{
   name: "Yair",
@@ -23,8 +22,10 @@ const initialData = [{
 ]
 
 export function DropdownAman() {
+  const inputRef = useRef();
   const [selectedCaller, setSelectedCaller] = useState()
   const [name, setName] = useState(" ");
+  const [nameEdit, setNameEdit] = useState(" ");
   const [dataUser, setDataUser] = useLocalStorage('users', initialData)
 
   const availableData = dataUser?.find(
@@ -33,6 +34,9 @@ export function DropdownAman() {
 
   const handleChange = (e) => {
     setName(e.target.value.trim());
+  };
+  const handleChangeEdit = (e) => {
+    setNameEdit(e.target.value.trim());
   };
 
   const handleSubmit = (e) => {
@@ -49,13 +53,42 @@ export function DropdownAman() {
     setDataUser(newDataUser);
   };
 
+
+  const handleEdit = (caller) => {
+    console.log('caller :>> ', caller);
+
+    const date = new Date
+    var options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    };
+
+
+    const newDataUser = dataUser.map(obj => {
+      if (obj.name == caller.name) {
+        return {"name": nameEdit, "date": date.toLocaleString("ru", options)};
+      } else {
+        return obj;
+
+      }
+    })
+    console.log('newDataUser :>> ', newDataUser);
+    setDataUser(newDataUser)
+  }
+
+  const handleDelete = (caller) => {
+    const newArr = dataUser.filter(elem => elem.name !== caller.name);
+    setDataUser(newArr)
+  };
+
   return (
     <>
 
       <Container maxWidth="sm">
-
-        <h2> Dropdown Aman </h2>
-
+        <h2> Dropdown Callers Dates </h2>
         <TextField
           name="name"
           onChange={handleChange}
@@ -65,12 +98,9 @@ export function DropdownAman() {
           focused
         />
 
-        <br /><br />
-        <Button variant="contained" onClick={handleSubmit}>Submit</Button>
-        <br /><br />
+        <Button sx={{m: 1}} variant="contained" onClick={handleSubmit}>Submit</Button>
 
-
-        <Box >
+        <Box sx={{m: 1}}>
           <FormControl sx={{width: "160px"}}          >
             <InputLabel  >Caller</InputLabel>
             <Select
@@ -96,12 +126,32 @@ export function DropdownAman() {
 
           <h1>{availableData?.date}</h1>
 
-          {/* {availableData &&
-            <Button variant="contained" onClick={handleDelete}>Delete</Button>
-          } */}
+          {availableData &&
+            <span>
+
+              <TextField
+                name="name"
+                onChange={handleChangeEdit}
+                label="new name"
+                variant="standard"
+                color="secondary"
+                focused
+              />
+
+              <Button sx={{mr: 1}}
+                color="success" variant="contained"
+                onClick={() => handleEdit(availableData)}>Edit
+              </Button>
+
+            </span>
+          }
+
+
+          {availableData &&
+            <Button sx={{mr: 1}} color="error" variant="contained" onClick={() => handleDelete(availableData)}>Delete</Button>
+          }
 
         </div>
-
 
       </Container>
     </>
